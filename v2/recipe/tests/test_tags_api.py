223143f45ea2dbd1240a1dd5462eacb215c1a61e
Fabio Rodrigues <fabio.rodrigues018@gmail.com>
2020-05-13 15:52:51 -0300

    creating tags

--- v2/recipe/tests/test_tags_api.py
+++ v2/recipe/tests/test_tags_api.py
@@ -57,3 +57,23 @@ class PrivatetagsApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
+
+    def test_create_tag_succesful(self):
+        payload = {'name':'Healthy'}
+        res = self.client.post(TAGS_URL, payload)
+        
+        exists = Tag.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+
+        self.assertTrue(exists)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+    def test_create_tag_invalid(self):
+        payload={'name':''}
+        res = self.client.post(TAGS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+
